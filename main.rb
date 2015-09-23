5244814bec42103b9f6288ff038411d8ac7ce5fd
AkinobuKatou <e5843435@yahoo.co.jp>
2015-09-23 05:14:55 +0000

    commit

--- main.rb
+++ main.rb
@@ -1,5 +1,14 @@
 require 'rubygems'
 require 'sinatra'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    "adapter" => "sqlite3",
+    "database" => "./bbs.db"
+)
+
+class Comment < ActiveRecord::Base
+end 
 
 helpers do
     def strong(s)
@@ -8,10 +17,12 @@ helpers do
 end
 
 get '/' do
-    "hello world"
+  @comments = Comment.order("id desc").all
+  erb :index
 end
 
 get '/:name' do |name|
   @name = name
+  @comments = Comment.order("id desc").all
   erb :name
 end
